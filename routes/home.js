4c29bcd64bce8808ad124895fc1f6ab1c1fe500d
Alexandro <parashnoktf@gmail.com>
2021-01-13 14:34:40 +0200

    Posts were fetched on home page, but it is still little data

--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,26 @@
 const {Router} = require('express')
 const router = Router()
 const {isAuthorized} = require('../middlewares/auth')
+const Post = require('../models/post')
 
-router.get('/', isAuthorized, (req, res) => {
-	res.render('home', {
-		title: 'Головна сторінка',
-		isHome: true,
-		success: req.flash('success'),
-		user: req.user.toObject()
-	})
+router.get('/', isAuthorized, async (req, res) => {
+	try {
+		const posts = await Post
+			.find()
+			.populate('userId', 'name avatarUrl')
+			.sort({date: 'desc'})
+			.lean()
+
+		res.render('home', {
+			title: 'Головна сторінка',
+			isHome: true,
+			success: req.flash('success'),
+			user: req.user.toObject(),
+			posts
+		})
+	} catch (e) {
+		console.trace(e)
+	}
 })
 
 module.exports = router
